ce5bde7813aedde9bb1e041300697eb754a56aad
YashGaur000 <103846663+YashGaur000@users.noreply.github.com>
2023-08-02 22:18:27 +0530

    changes in operations

--- scripts/services/product-operations.js
+++ scripts/services/product-operations.js
@@ -1,3 +1,6 @@
+import Product from "../Models/product";
+import doNetworkCall from "./api-client";
+
 const productOperations={
     sort(){
 
@@ -5,8 +8,15 @@ const productOperations={
     search(){
 
     },
-    readAllProducts(){
-        doNetworkCall();
+    async readAllProducts(){
+        const object = await doNetworkCall();
+        const pizzas = await object['Vegetatrian'];
+        const products=[];
+        for(let pizza of pizzas){
+            const product = new Product(
+                pizza.id, pizza.name, pizza.menu_description, pizza.price, pizza.assets.product_details_page[0].url)
+                products.push(product);
+        }return products;
     }
 }
 export default productOperations;
